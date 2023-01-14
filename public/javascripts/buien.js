91c8fe5e697358ee2b27f3d3a97f197da19f2b97
Jan Willem Boer <j.w.boer@saxion.nl>
2023-01-14 13:19:46 +0100

    tekst bij buien ietsje anders

--- public/javascripts/buien.js
+++ public/javascripts/buien.js
@@ -13,6 +13,7 @@ if (buienData.length > 0) {
 
     let firstPeriod;
     let periodPrecip = 0;
+    let numPeriodsWithRain = 0;
 
     for (let i = 0; i < buienData.length; i++) {
         const periodData = buienData[i];
@@ -23,10 +24,14 @@ if (buienData.length > 0) {
         context.fillRect(i * barSize + 1, height - barHeight - 2, barSize - 2, barHeight);
 
         if (precip > 0) {
-            periodPrecip += Math.round(precip) / 10;
+            periodPrecip += precip / 10;
             if (!firstPeriod && periodPrecip > 0.0) {
                 firstPeriod = period;
             }
+        }
+
+        if (firstPeriod) {
+            numPeriodsWithRain++;
         }
     }
 
@@ -36,10 +41,21 @@ if (buienData.length > 0) {
     context.closePath();
     context.stroke();
 
-    if (firstPeriod) {
-        buienTekst.innerText = periodPrecip.toLocaleString("nl-NL") + " mm neerslag verwacht (vanaf " + firstPeriod + ")";
+    if (numPeriodsWithRain > 0) {
+        let rain;
+        if (numPeriodsWithRain > 12) {
+            // do one hour in advance
+            const averagePer5Min = periodPrecip / numPeriodsWithRain;
+            const mmPerHour = Math.round(averagePer5Min * 12 * 10) / 10;
+            rain = "gemiddeld " + mmPerHour.toLocaleString("nl-NL") + " mm/u";
+        } else {
+            periodPrecip = Math.round(periodPrecip * 10) / 10;
+            rain = "totaal " + periodPrecip.toLocaleString("nl-NL") + " mm";
+        }
+
+        buienTekst.innerText = "Regen vanaf " + firstPeriod + ", " + rain;
     } else {
-        buienTekst.innerText = "Geen neerslag verwacht.";
+        buienTekst.innerText = "Komende uur geen neerslag verwacht.";
     }
 
 } else {
